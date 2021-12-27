4bb8f7362e34925ffca811be6faf3cadefc4b462
Davidson031 <davidson031@hotmail.com>
2021-12-27 15:33:24 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/davidson/cursomc/services/CategoriaService.java
+++ src/main/java/com/davidson/cursomc/services/CategoriaService.java
@@ -1,11 +1,16 @@
 package com.davidson.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 import com.davidson.cursomc.domain.Categoria;
 import com.davidson.cursomc.repositories.CategoriaRepository;
+import com.davidson.cursomc.services.exceptions.DateIntegrityException;
 import com.davidson.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,13 +19,39 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	
-	public Categoria buscar (Integer id) { 
+	public Categoria find (Integer id) { 
 		
 		Optional<Categoria> obj = repo.findById(id); 
-		
-		
-		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 	} 
+	
+	public Categoria insert(Categoria obj) {
+		
+		obj.setId(null);
+		return repo.save(obj);
+		
+	}
+	
+	public Categoria update(Categoria obj) {
+		
+		find(obj.getId());
+		return repo.save(obj);
+		
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+		
+	}
 
 }
--- src/main/java/com/davidson/cursomc/services/ClienteService.java
+++ src/main/java/com/davidson/cursomc/services/ClienteService.java
@@ -14,11 +14,9 @@ public class ClienteService {
 	private ClienteRepository repo;
 	
 	
-	public Cliente buscar (Integer id) { 
+	public Cliente find (Integer id) { 
 		
 		Optional<Cliente> obj = repo.findById(id); 
-		
-		
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
 	} 
